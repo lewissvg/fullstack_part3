4c014fde4f69061c814e28e99802f15b535d6d74
lewisbrotchie <lewis@yulife.com>
2019-09-11 15:40:34 +0100

    exercise 3.5 completed

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
 
 let persons = [
   {
-    id: 1,
     name: "Arto Hellas",
-    number: "040-123456"
+    number: "040-123456",
+    id: 1
   },
   {
-    id: 2,
     name: "Ada Lovelace",
-    number: "39-44-5323523"
+    number: "39-44-5323523",
+    id: 2
   },
   {
-    id: 3,
     name: "Dan Abramov",
-    number: "12-43-234345"
+    number: "12-43-234345",
+    id: 3
   },
   {
-    id: 4,
     name: "Mary Poppendieck",
-    number: "39-23-6423122"
+    number: "39-23-6423122",
+    id: 4
   }
 ];
 
@@ -49,11 +52,20 @@ app.get("/api/persons/:id", (req, res) => {
   }
 });
 
-app.delete("/persons/:id", (req, res) => {
+app.delete("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
   persons = persons.filter(person => person.id !== id);
 
   res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const person = req.body;
+  person.id = Math.floor(Math.random() * 1000000);
+
+  persons = persons.concat(person);
+
+  res.json(person);
 });
 
 const PORT = 3001;
