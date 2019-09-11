60e5abc9920f68d04314c66ad0dd2ad37af292f7
lewisbrotchie <lewis@yulife.com>
2019-09-11 14:04:33 +0100

    exercise 3.2 completed

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 
+app.get("/info", (req, res) => {
+  res.send(
+    `Phonebook has info for ${persons.length} people <p>${new Date()}</p>`
+  );
+});
+
 app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
