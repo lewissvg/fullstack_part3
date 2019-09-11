3d7571bd45167b4f706f9f281c4ab4fe16598f29
lewisbrotchie <lewis@yulife.com>
2019-09-11 15:17:25 +0100

    exercise 3.3 completed

--- index.js
+++ index.js
@@ -38,6 +38,17 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find(persons => persons.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
